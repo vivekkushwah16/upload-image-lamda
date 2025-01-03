3475c2a31b7fb23fbd4b73b0fb64e00b7baba699
Vivek Kushwah <vivekkushwah714@gmail.com>
2025-01-03 19:28:39 +0530

    modified acc to lamda

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
-import express from "express";
-import multer from "multer";
+import axios from "axios";
 import { allowedMimeTypes } from "./constants/index.js";
 import { validateFiles } from "./utils/index.js";
-import axios from "axios";
-import dotenv from "dotenv";
-import cors from "cors";
 
-dotenv.config();
-const app = express();
-const upload = multer({ storage: multer.memoryStorage() });
-const whiteListURLS = [
-  "http://localhost:5173",
-  "http://app.dev.defhawk.com.s3-website.ap-south-1.amazonaws.com",
-  "https://defhawk.com",
-  "https://app.defhawk.com",
-];
-
-const corsOpts = {
-  credentials: true,
-  methods: "GET,PUT,POST,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization",
-  origin: function (origin, callback) {
-    if (whiteListURLS.indexOf(origin || "") !== -1 || origin === undefined) {
-      callback(null, true);
-    } else {
-      callback(new Error("Unauthorized Domain"), false);
-    }
-  },
-};
-
-app.use(cors(corsOpts)); // Apply CORS middleware
-app.options("*", cors(corsOpts));
-
-app.post("/upload-avatar", upload.single("file"), async (req, res) => {
+async function uploadAsset(req) {
   try {
-  
     const { file } = req;
     const { folder, fileName, fileType } = req.body;
     // Validate the file MIME type
     const isValid = await validateFiles({ file, fileType }, allowedMimeTypes);
 
     if (!isValid) {
-      return res.status(400).send({error:"Invalid file type",data});
+      return res.status(400).send({ error: "Invalid file type", data });
     }
 
     // Step 1: Get the presigned URL from your API (replace the URL with your actual API endpoint)
@@ -71,16 +40,17 @@ app.post("/upload-avatar", upload.single("file"), async (req, res) => {
     }
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .send({
-        data: null,
-        error: error.response?.data?.error || error.response?.data?.message,
-      });
+    res.status(500).send({
+      data: null,
+      error: error.response?.data?.error || error.response?.data?.message,
+    });
   }
-});
+}
 
-// Start the server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+export const handler = async function (req) {
+  try {
+    await uploadAsset(req)
+  } catch (error) {
+    console.log("Error processing file upload")
+  }
+}
